1bf8862081dff89e5e24a2eaeecf356f56ca2303
Hong <hong.cao0824@gmail.com>
2024-10-22 15:54:30 +0100

    update to drf authentication

--- wetrack_backend/auth_app/urls.py
+++ wetrack_backend/auth_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
-from users.views import UserDetailView
+from .views import UserDetailView
 
 urlpatterns = [
-    # ...
-    path('auth/', include('dj_rest_auth.urls')),
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include('dj_rest_auth.urls')),  # This includes login and logout
+    path('registration/', include('dj_rest_auth.registration.urls')),
+
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('users/me/', UserDetailView.as_view(), name='user-me'),
     path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
 ]
