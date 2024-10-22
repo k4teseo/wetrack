7c6ee8ca4fcba9168ac30cf617e5d666b104dcd5
Hong <hong.cao0824@gmail.com>
2024-10-22 15:56:06 +0100

    update user detail view

--- wetrack_backend/auth_app/views.py
+++ wetrack_backend/auth_app/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
-
-# Create your views here.
+# auth_app/views.py
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import serializers
 from .models import CustomUser
-from .serializers import CustomUserSerializer
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')
+        extra_kwargs = {
+            'email': {'required': False},
+            'username': {'required': True}
+        }
 
 class UserDetailView(generics.RetrieveUpdateAPIView):
     queryset = CustomUser.objects.all()
-    serializer_class = CustomUserSerializer
+    serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+        if pk == 'me':
+            return self.request.user
+        return super().get_object()
