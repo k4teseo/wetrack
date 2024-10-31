46524ce3ad958e9fc4ce66c53611ef8406a43903
Kate Seo <kateseo@bu.edu>
2024-10-31 15:48:28 +0000

    adding navigation

--- wetrack_mobile/App.tsx
+++ wetrack_mobile/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./app/(auth)/login.tsx";
 import Signup from "./app/(auth)/signup.tsx";
+import { enableScreens } from 'react-native-screens';
+
+enableScreens();
+
+const Stack = createNativeStackNavigator();
 
 function App() {
     return(
-        <Signup/>
-        )
+        <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Signup" component={Signup} />
+        </Stack.Navigator>
+        </NavigationContainer>
+        );
     }
 
 export default App;
