6df588e539d116df9a51d8f92f7d2593d1e9562d
Kate Seo <kateseo@bu.edu>
2024-10-31 15:53:29 +0000

    fixed navigation and added everything to src

--- wetrack_mobile/src/App.tsx
+++ wetrack_mobile/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from "./app/(auth)/login.tsx";
-import Signup from "./app/(auth)/signup.tsx";
+import Login from "../app/(auth)/login.tsx";
+import Signup from "../app/(auth)/signup.tsx";
 import { enableScreens } from 'react-native-screens';
 
 enableScreens();
