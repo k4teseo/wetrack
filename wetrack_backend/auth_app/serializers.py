155db828f134f777599017f21cefb78da10bb84b
Hong <hong.cao0824@gmail.com>
2024-11-05 16:24:04 +0000

    support for linking

--- wetrack_backend/auth_app/serializers.py
+++ wetrack_backend/auth_app/serializers.py
@@ -1,7 +1,21 @@
+# auth_app/serializers.py
 from rest_framework import serializers
 from .models import CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(required=True)  # Add this field
+
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'date_of_birth', 'country')
+        fields = ('id', 'username', 'email', 'name', 'password1', 'password2')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'email': {'required': False},
+        }
+
+    def create(self, validated_data):
+        name = validated_data.pop('name', '')  # Get the name
+        user = CustomUser.objects.create_user(**validated_data)
+        user.first_name = name  # Store the name in first_name field
+        user.save()
+        return user
--- wetrack_backend/auth_app/views.py
+++ wetrack_backend/auth_app/views.py
@@ -3,6 +3,21 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import serializers
 from .models import CustomUser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import CustomUserSerializer
+
+@api_view(['POST'])
+def register_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        return Response({
+            'user': UserSerializer(user).data,
+            'message': 'User registered successfully'
+        }, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
