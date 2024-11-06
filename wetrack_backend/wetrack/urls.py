10a8702184023aa506c7b055afe57a7be53cf6b8
Hong <hong.cao0824@gmail.com>
2024-11-06 16:07:56 +0000

    update urls

--- wetrack_backend/wetrack/urls.py
+++ wetrack_backend/wetrack/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('dj_rest_auth.urls')),
     path('api/auth/', include('auth_app.urls')),
     path('auth/registration/', include('dj_rest_auth.registration.urls')),
    # path('api/currency/', include('currency.urls')),
