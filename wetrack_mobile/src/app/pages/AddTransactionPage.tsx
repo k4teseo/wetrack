c32f2fd02ced4d4d0f3fbc9486a3b4b4f87f5b56
Kate Seo <kateseo@bu.edu>
2024-11-06 15:58:38 +0000

    adding all the internal pages to prepare for building and styling

---
+++ wetrack_mobile/src/app/pages/AddTransactionPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const AddTransactionPage = () => {
+    return (
+        <Text>Add Transaction</Text>
+        );
+}
+
+export default AddTransactionPage;
