321353e979007dbf003882ff0ef4fb1e8a87969a
Hong <hong.cao0824@gmail.com>
2024-11-13 16:03:23 +0000

    login leads to page properly

--- wetrack_mobile/src/App.tsx
+++ wetrack_mobile/src/App.tsx
@@ -1,45 +1,32 @@
 import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from "./app/(auth)/login.tsx";
-import Signup from "./app/(auth)/signup.tsx";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { enableScreens } from 'react-native-screens';
-import DashboardPage from './app/pages/DashboardPage.tsx';
-import TransactionPage from './app/pages/TransactionPage.tsx';
-import AddTransactionPage from './app/pages/AddTransactionPage.tsx';
-import BudgetPage from './app/pages/BudgetPage.tsx';
-import ProfilePage from './app/pages/ProfilePage.tsx';
+import { View, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+// Auth imports
+import Login from "../src/app/(auth)/login";
+import Signup from "../src/app/(auth)/signup";
+
+// Page imports
+import DashboardPage from '../src/app/pages/DashboardPage';
+import TransactionPage from '../src/app/pages/TransactionPage';
+import AddTransactionPage from '../src/app/pages/AddTransactionPage';
+import BudgetPage from '../src/app/pages/BudgetPage';
+import ProfilePage from '../src/app/pages/ProfilePage';
+
+// Asset imports
 import dashboard from './assets/icons/Dashboard.png';
 import transactions from './assets/icons/CurrencyCircleDollar.png';
 import add from './assets/icons/Plus.png';
 import budget from './assets/icons/PiggyBank.png';
 import user from './assets/icons/User.png';
 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/Ionicons';
-import { View, TouchableOpacity, StyleSheet, Image, Text } from 'react-native';
-
 enableScreens();
 
 const Stack = createNativeStackNavigator();
-
-function DashboardScreen() {
-    return <DashboardPage />;
-}
-function TransactionsScreen() {
-    return <TransactionPage />;
-}
-function AddScreen() {
-    return <AddTransactionPage />;
-}
-function BudgetScreen() {
-    return <BudgetPage />;
-}
-function ProfileScreen() {
-    return <ProfilePage />;
-}
-
-// Define Tab Navigator
 const Tab = createBottomTabNavigator();
 
 function CustomTabBarButton({ children, onPress }) {
@@ -55,114 +42,122 @@ function CustomTabBarButton({ children, onPress }) {
     );
 }
 
+function TabNavigator() {
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                tabBarShowLabel: true,
+                tabBarStyle: { height: 78 },
+                headerShown: false,
+            }}
+        >
+            <Tab.Screen
+                name="Dashboard"
+                component={DashboardPage}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Image
+                            source={dashboard}
+                            style={{
+                                width: 38,
+                                height: 38,
+                            }}
+                        />
+                    ),
+                    tabBarLabelStyle: {
+                        fontSize: 12,
+                        color: 'black',
+                    },
+                }}
+            />
+            <Tab.Screen
+                name="Transactions"
+                component={TransactionPage}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Image
+                            source={transactions}
+                            style={{
+                                width: 38,
+                                height: 38,
+                            }}
+                        />
+                    ),
+                    tabBarLabelStyle: {
+                        fontSize: 12,
+                        color: 'black',
+                    },
+                }}
+            />
+            <Tab.Screen
+                name="Add"
+                component={AddTransactionPage}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <Icon name="add" color="#333" size={30} />
+                    ),
+                    tabBarButton: (props) => (
+                        <CustomTabBarButton {...props}>
+                            <Image source={add} />
+                        </CustomTabBarButton>
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Budget"
+                component={BudgetPage}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Image
+                            source={budget}
+                            style={{
+                                width: 38,
+                                height: 38,
+                            }}
+                        />
+                    ),
+                    tabBarLabelStyle: {
+                        fontSize: 12,
+                        color: 'black',
+                    }
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={ProfilePage}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Image
+                            source={user}
+                            style={{
+                                width: 38,
+                                height: 38,
+                            }}
+                        />
+                    ),
+                    tabBarLabelStyle: {
+                        fontSize: 12,
+                        color: 'black',
+                    }
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
+
 function App() {
-    return(
-//          <NavigationContainer>
-//             <Stack.Navigator initialRouteName="Login">
-//                 <Stack.Screen name="Login" component={Login} />
-//                 <Stack.Screen name="Signup" component={Signup} />
-//                 <Stack.Screen name="Profile" component={ProfilePage} />
-//             </Stack.Navigator>
-//         </NavigationContainer>
+    return (
         <NavigationContainer>
-            <Tab.Navigator
+            <Stack.Navigator
+                initialRouteName="Login"
                 screenOptions={{
-                    tabBarShowLabel: true,
-                    tabBarStyle: { height: 78 },
-                    headerShown: false,
+                    headerShown: false
                 }}
             >
-                <Tab.Screen
-                    name="Dashboard"
-                    component={DashboardScreen}
-                    options={{
-                        tabBarIcon: ({ color, size }) => (
-                            <Image
-                                source={dashboard}
-                                style={{
-                                    width: 38,
-                                    height: 38,
-                                }}
-                            />
-                        ),
-                        tabBarLabelStyle: {
-                            fontSize: 12,
-                            color: 'black',
-                        },
-                    }}
-                />
-                <Tab.Screen
-                    name="Transactions"
-                    component={TransactionsScreen}
-                    options={{
-                        tabBarIcon: ({ color, size }) => (
-                            <Image
-                                source={transactions}
-                                style={{
-                                    width: 38,
-                                    height: 38,
-                                }}
-                            />
-                        ),
-                        tabBarLabelStyle: {
-                            fontSize: 12,
-                            color: 'black',
-                        },
-                    }}
-                />
-                <Tab.Screen
-                    name="Add"
-                    component={AddScreen}
-                    options={{
-                        tabBarIcon: ({ focused }) => (
-                            <Icon name="add" color="#333" size={30} />
-                        ),
-                        tabBarButton: (props) => (
-                            <CustomTabBarButton {...props}>
-                                <Image source={add} />
-                            </CustomTabBarButton>
-                        ),
-                    }}
-                />
-                <Tab.Screen
-                    name="Budget"
-                    component={BudgetScreen}
-                    options={{
-                        tabBarIcon: ({ color, size }) => (
-                            <Image
-                                source={budget}
-                                style={{
-                                    width: 38,
-                                    height: 38,
-                                }}
-                            />
-                        ),
-                        tabBarLabelStyle: {
-                            fontSize: 12,
-                            color: 'black',
-                        }
-                    }}
-                />
-                <Tab.Screen
-                    name="Profile"
-                    component={ProfileScreen}
-                    options={{
-                        tabBarIcon: ({ color, size }) => (
-                            <Image
-                                source={user}
-                                style={{
-                                    width: 38,
-                                    height: 38,
-                                }}
-                            />
-                        ),
-                        tabBarLabelStyle: {
-                            fontSize: 12,
-                            color: 'black',
-                        }
-                    }}
-                />
-            </Tab.Navigator>
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Signup" component={Signup} />
+                <Stack.Screen name="MainApp" component={TabNavigator} />
+            </Stack.Navigator>
         </NavigationContainer>
     );
 }
